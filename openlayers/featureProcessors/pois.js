f18acd7c6860e08c3d9b5fb727934b5cf1bf6025
Johannes Novotny <jonovotny@users.noreply.github.com>
2026-03-26 15:03:24 +0100

    Update pois.js

--- openlayers/featureProcessors/pois.js
+++ openlayers/featureProcessors/pois.js
@@ -15,31 +15,32 @@ const math = create(all, {});
 
 import {  lineIntersect, area, bezierSpline, concave, bboxPolygon, booleanWithin, bbox, pointToPolygonDistance, tin, multiPoint, explode, lineChunk, simplify, flatten, booleanTouches, multiPolygon, booleanPointOnLine, cleanCoords, polygonSmooth, clone, combine, featureCollection, multiLineString, polygon, truncate, point, lineString, lineOffset, polygonToLine, lineToPolygon, unkinkPolygon, booleanClockwise, rewind, lineSplit, length, along, pointToLineDistance, booleanIntersects, lineSliceAlong, voronoi, intersect, booleanPointInPolygon, angle } from '@turf/turf';
 
-var svgMarkers = {};
-svgMarkers['City'] = '<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+var markerDetails = {};
+markerDetails['City'] = {src: 'data:image/svg+xml;utf8,<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
     + '<circle style="fill:black;stroke:black;stroke-width:1.5" r="1.3" cx="5" cy="5"/>'
-    + '</svg>';
-svgMarkers['Port'] = '<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    + '</svg>',
+	minZoom: 5};
+markerDetails['Port'] = {src: 'data:image/svg+xml;utf8,<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
     + '<circle style="fill:white;fill-opacity:0.4;stroke:black;stroke-width:1.5" r="3.5" cx="5" cy="5"/>'
-    + '</svg>';
-svgMarkers['Ruin'] = '<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    + '</svg>'};
+markerDetails['Ruin'] = {src: 'data:image/svg+xml;utf8,<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
     + '<rect style="fill:black;stroke:black;stroke-width:1.5" width="6" height="6" x="2" y="2"/>'
-    + '</svg>';
-svgMarkers['Fortress'] = '<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    + '</svg>'};
+markerDetails['Fortress'] = {src: 'data:image/svg+xml;utf8,<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
     + '<rect style="fill:white;stroke:black;stroke-width:1.5" width="6" height="6" x="2" y="2"/>'
-    + '</svg>';
-svgMarkers['Capital'] = '<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    + '</svg>'};
+markerDetails['Capital'] = {src: 'data:image/svg+xml;utf8,<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
     + '<path style="fill:black;stroke:black;stroke-width:1.5" d="M 5.80303 6.05219 L 5.01593 5.66099 L 4.24746 6.0876 L 4.37629 5.21815 L 3.73309 4.61911 L 4.5998 4.47296 L 4.97075 3.67613 L 5.37758 4.45526 L 6.25005 4.56182 L 5.63477 5.1895 Z" />'
-    + '</svg>';
-svgMarkers['Site'] = '<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    + '</svg>'};
+markerDetails['Site'] = {src: 'data:image/svg+xml;utf8,<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
     + '<path style="fill:none;stroke:black;stroke-width:1.5" d="M 2.7 2.7 L 7.3 7.3 M 2.7 7.3 L 7.3 2.7" />'
-    + '</svg>';
-svgMarkers['Temple'] = '<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    + '</svg>'};
+markerDetails['Temple'] = {src: 'data:image/svg+xml;utf8,<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
     + '<polygon style="fill:black;stroke:black;stroke-width:1.5" points="5,3 6.73,6.46 3.17,6.46" />'
-    + '</svg>';
-svgMarkers['Bridge'] = '<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    + '</svg>'};
+markerDetails['Bridge'] = {src: 'data:image/svg+xml;utf8,<svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">'
     + '<path style="fill:black;stroke:black;stroke-width:1.5" d="M 2.01892 1.48946 L 2.99774 2.46822 L 2.99774 7.53179 L 2.01892 8.51055 M 7.98173 8.51055 L 7.00291 7.53179 L 7.00291 2.46822 L 7.98173 1.48946" />'
-    + '</svg>';
+    + '</svg>'};
 
 function createMarkerStyle (types, rotation) {
 	var typeName = "Marker " + types;
@@ -54,12 +55,12 @@ function createMarkerStyle (types, rotation) {
 	for (var type of types){
 		style.push(
 			new Style({
-				image: new Icon({
+				image: new Icon(Object.assign({
 					opacity: 1,
-					src: 'data:image/svg+xml;utf8,' + svgMarkers[type],
+					src: 'data:image/svg+xml;utf8,',
 					scale: 1.5,
 					rotation: rotation
-				})
+				}, markerDetails[type]))
 			})
 		)
 	}
@@ -76,13 +77,12 @@ function createMarkerStyle (types, rotation) {
 }
 
 function createLabelStyle(text, types, resolution) {
-	console.log(label);
-	var label = label.match(/^(.*)\s\(/)[1];
-	var typeName = "POI Label " + label;
+	console.log(text);
+	var typeName = "POI Label " + text;
 	if (styleLib[typeName]) return typeName;
 
 	var labelStyle = styleLib["[Gen] POI Labels"].clone();
-	labelStyle.getText().setText(label);
+	labelStyle.getText().setText(text);
 	styleLib[typeName] = labelStyle;
 	return typeName;
 }
@@ -101,8 +101,8 @@ function createPOIs(layerGroups, transform, features, exportFeatures){
 		var types = "Site";
 		var text = "";
 		if (tokens) {
-			text = types[1];
-			types = types[2];
+			text = tokens[1];
+			types = tokens[2];
 		} 
 
 		var orientation = 0;
